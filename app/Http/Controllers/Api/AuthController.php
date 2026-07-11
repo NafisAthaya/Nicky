b16d7ac586eeae7e8b2fde428040b05fae516ad2
NafisAthaya <nafisathaya22@gmail.com>
2026-07-12 02:42:33 +0700

    Fix login without password_reset_requests table 2 kali

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use App\Models\PasswordResetRequest;
+//use App\Models\PasswordResetRequest;
 use App\Models\Notifikasi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
